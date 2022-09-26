3f2712b8fa86ccc74f00cde7397b67d355e6b1a0
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2022-09-26 21:52:04 +0200

    leading e trailing

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -13,6 +13,26 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(child: Image.network(imageUrl));
+    return GridTile(
+      footer: GridTileBar(
+        leading: IconButton(
+          icon: const Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        trailing: IconButton(
+          icon: const Icon(Icons.shopping_cart),
+          onPressed: () {},
+        ),
+        backgroundColor: Colors.black54,
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+      ),
+      child: Image.network(
+        imageUrl,
+        fit: BoxFit.cover,
+      ),
+    );
   }
 }
