4a5c6f10a2f79c4d56e839faeb543245cf94b554
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2022-09-27 15:51:20 +0200

    fonts

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'MyShop',
       theme: ThemeData(
+          fontFamily: 'Lato',
           colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
               .copyWith(secondary: Colors.deepOrange)),
       home: ProductsOverviewScreen(),
