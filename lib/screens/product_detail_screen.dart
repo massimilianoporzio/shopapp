964a20860d1487df83375d20359db0246020d4b5
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2022-09-28 14:23:13 +0200

    details prendono da provider

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopapp/providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = 'product-detail';
@@ -7,9 +9,12 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context)!.settings.arguments as String;
+    final loadedProduct = Provider.of<Products>(context)
+        .items
+        .firstWhere((element) => element.id == productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text('prodotto con id: $productId'),
+        title: Text(loadedProduct.title),
       ),
     );
   }
