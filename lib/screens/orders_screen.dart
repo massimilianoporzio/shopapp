9e829922fe6f0ac9346010cb2dbf0bab31471e9a
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2022-10-01 15:23:55 +0200

    OrderItems impoistato

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopapp/widgets/order_item.dart';
+
+import '../providers/orders.dart' show Orders; //*import only Orders
 
 class OrdersScreen extends StatelessWidget {
   const OrdersScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final orderData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(title: const Text('Your orders')),
+      body: ListView.builder(
+        itemCount: orderData.orders.length,
+        itemBuilder: (context, index) =>
+            OrderItem(order: orderData.orders[index]),
+      ),
     );
   }
 }
---
+++ lib/widgets/order_item.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../providers/orders.dart' as ord;
+
+class OrderItem extends StatelessWidget {
+  final ord.OrderItem order;
+  const OrderItem({super.key, required this.order});
+
+  @override
+  Widget build(BuildContext context) {
+    final formatCurrency = NumberFormat.simpleCurrency();
+    return Card(
+      margin: const EdgeInsets.all(10),
+      child: Column(children: [
+        ListTile(
+          title: Text(formatCurrency.format(order.amount)),
+          subtitle:
+              Text(DateFormat('dd/MM/yyyy hh:mm').format(order.orderDate)),
+          trailing: IconButton(
+            icon: const Icon(Icons.more),
+            onPressed: () {},
+          ),
+        )
+      ]),
+    );
+  }
+}
