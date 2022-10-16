6b940c30eda269b179202c55c6c22724d5028c95
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2022-10-16 22:45:53 +0200

    env

---
+++ lib/env/env.dart
@@ -0,0 +1,10 @@
+// lib/env/env.dart
+import 'package:envied/envied.dart';
+
+part 'env.g.dart';
+
+@Envied(path: '.env')
+abstract class Env {
+  @EnviedField(varName: 'FIREBASE', obfuscate: true)
+  static const firebase = _Env.firebase;
+}
