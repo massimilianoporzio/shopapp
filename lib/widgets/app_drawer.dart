9a07b230a88b9e74fc40593a13bfa2610186fc2f
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2022-10-23 17:57:34 +0200

    logout

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shopapp/screens/orders_screen.dart';
 import 'package:shopapp/screens/product_detail_screen.dart';
 import 'package:shopapp/screens/products_overview_screen.dart';
 import 'package:shopapp/screens/user_products_screen.dart';
+
+import '../providers/auth.dart';
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({super.key});
@@ -40,7 +43,14 @@ class AppDrawer extends StatelessWidget {
             Navigator.of(context)
                 .pushReplacementNamed(UserProductsScreen.routeName);
           },
-        )
+        ),
+        const Divider(),
+        ListTile(
+            leading: const Icon(Icons.exit_to_app),
+            title: const Text('Logout'),
+            onTap: () {
+              Provider.of<Auth>(context, listen: false).logout();
+            })
       ]),
     );
   }
